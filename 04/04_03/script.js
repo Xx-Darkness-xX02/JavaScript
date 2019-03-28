7361f011c85ba5ba7243f1534a3156a230735a7d
Xx-Darkness-xX02 <kjell.roelofs@student.rocvf.nl>
2019-03-28 14:01:53 +0100

    hoofdstuk 4

---
+++ 04/04_03/script.js
@@ -0,0 +1,9 @@
+function findBiggestFraction(a,b) {
+    a>b ? console.log("a: ", a) : console.log("b: ", b);
+}
+
+var firstFraction = 3/4;
+var secondFraction = 5/7;
+
+findBiggestFraction(firstFraction, secondFraction);
+findBiggestFraction(6/12, 10/12)
