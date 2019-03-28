a9aa912bf66693e24ca9635582b8a43e46323e9b
Xx-Darkness-xX02 <kjell.roelofs@student.rocvf.nl>
2019-03-28 11:10:24 +0100

    hoofdstuk 2 en 3

---
+++ 03/03_04/script.js
@@ -0,0 +1,6 @@
+var a = 5;
+var b = "4";
+var sum = a + b;
+
+var humanSum = "the sum of a:" + a + "the sum of b:" + b + "the sum of a and b is:" + sum + ".";
+console.log(humanSum);
---
+++ 03/03_07/script.js
@@ -0,0 +1,7 @@
+var pens;
+pens = ["red", "green", "blue", "orange"]
+
+
+var mixedBag;
+mixedBag = ["hello", 5, false, 534, "one more thing"]
+console.log(mixedBag);
